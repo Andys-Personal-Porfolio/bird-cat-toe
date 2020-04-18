bb21418dabdeb958bcfa2ae0ea7b327b66328adf
Andy Tom <andrew.tom@colorado.edu>
2020-04-18 09:16:37 -0600

    Initial Commit

---
+++ player.js
@@ -0,0 +1,16 @@
+
+class Player {
+  constructor(id) {
+    this.id = id;
+    this.token = 'one';
+    this.wins = [];
+  }
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+}
+module.exports = Player;
