6996e0565b3729a71945f5d6cc77126d4d2c8696
Andy Tom <andrew.tom@colorado.edu>
2020-04-19 15:18:27 -0600

    Separate updateDisplay and updateHeader

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 var game = new Game();
 var winner;
-function addToBoard(spot, player) {
-  game.board[spot] = player;
-  winner = game.checkScore();
-  console.log("game.board" ,game.board)
-  console.log('winner', winner)
+var gameBoard = document.querySelector('.game-board');
+
+var whoseTurn = 1;
+gameBoard.addEventListener('click',addToBoard)
+
+function addToBoard(event) {
+
+  if(!game.board[event.target.id] && !winner){
+      game.board[event.target.id] = whoseTurn;
+      winner = game.checkScore();
+      updateDisplay();
+  } else if (winner) {
+    game.resetGame();
+  }
+    updateHeader();
+
+}
+
+function updateDisplay() {
+  var spot = document.getElementById(event.target.id);
+  spot.innerText = whoseTurn;
+  whoseTurn = whoseTurn === 1 ? 2 : 1
+}
+
+function updateHeader() {
+    var turnHeader = document.querySelector('.turn-header');
+    turnHeader.innerText = winner ||`It is player ${whoseTurn}'s turn`
 }
