73dd6ae5f030c5e71342ebb364f0c132acf0e7c5
Andy Tom <andrew.tom@colorado.edu>
2020-04-19 18:32:53 -0600

    Add functionality to display past games

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ function addToBoard(event) {
       game.board[event.target.id] = whoseTurn;
       winner = game.checkScore();
       updateDisplay();
+
   } else if (winner) {
+    game.saveWin();
+    displayMini();
     game.resetGame();
   }
     updateHeader();
@@ -27,4 +30,33 @@ function updateDisplay() {
 function updateHeader() {
     var turnHeader = document.querySelector('.turn-header');
     turnHeader.innerText = winner ||`It is player ${whoseTurn}'s turn`
+}
+
+function displayMini(){
+  var miniGameBoard = document.getElementById('mini-game-board')
+  if(winner === 1){
+    var boards = game.playerOne.wins
+  } else if (winner === 2) {
+    var boards = game.playerTwo.wins
+  }
+  miniGameBoard.insertAdjacentHTML('beforeend', ` <section id = "single-mini-game">
+    <article class = "mini-spot">${boards[boards.length -1][0] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][1] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][2] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][3] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][4] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][5] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][6] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][7] || ' '}
+    </article>
+    <article class = "mini-spot">${boards[boards.length -1][8] || ' '}
+    </article>
+    </trial>`)
 }
